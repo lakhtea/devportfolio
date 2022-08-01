b13da2cd72ba3b47060fd24f0fccbe73c87d2664
Lakhte Ali Agha <lagha@ltvco.com>
2022-07-31 23:22:04 -0400

    header scrolling fixed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header/Header";
 import Content from "./components/Content/Content";
 import Menu from "./components/Menu/Menu";
 import { useState, createContext } from "react";
+import { isMobile } from "react-device-detect";
 
 export const MenuContext = createContext();
 
@@ -14,12 +15,16 @@ function App() {
   const handleMenuToggle = (close) => {
     close === true ? setMenuOpen(false) : setMenuOpen(!menuOpen);
     document.querySelector("body").classList.toggle("blur");
-    document.querySelector("html").classList.toggle("scrollbar-hidden");
+    console.log(isMobile);
+    if (!isMobile)
+      window.document
+        .querySelector("html")
+        .classList.toggle("scrollbar-hidden");
   };
 
   const handleClickOutside = (e) => {
     if (!e.target.classList.contains("menu-el") && menuOpen)
-      handleMenuToggle(null, true);
+      handleMenuToggle(true);
   };
 
   return (
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,58 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import Logo from "../Logo/Logo";
 import MenuIcon from "../MenuIcon/MenuIcon";
 import Nav from "../Nav/Nav";
+import { useScrollDirection } from "use-scroll-direction";
 import styles from "./Header.module.css";
+import { MenuContext } from "../../App";
 
 const Header = () => {
-  const { container } = styles;
+  const { container, down, up, extraPadding } = styles;
   const [isMobile, setIsMobile] = useState(window.innerWidth < 650);
+  const [isTop, setIsTop] = useState(true);
+  const [scrollingUp, setScrollingUp] = useState(false);
+  const { isScrollingUp, isScrollingDown } = useScrollDirection();
+  const { menuOpen } = useContext(MenuContext);
+
   const update = () => {
     setIsMobile(window.innerWidth < 650);
   };
 
+  const fixUnsetHeader = () => {
+    if (window.scrollY > 80) {
+      setIsTop(false);
+    }
+
+    if (window.scrollY !== 0 && isScrollingUp) {
+      setScrollingUp(true);
+    }
+
+    if (isScrollingDown) {
+      setScrollingUp(false);
+    }
+
+    if (window.scrollY === 0) {
+      setScrollingUp(true);
+      setIsTop(true);
+    }
+  };
+
   useEffect(() => {
     window.addEventListener("resize", update);
+    window.addEventListener("scroll", fixUnsetHeader);
+
+    return () => {
+      window.removeEventListener("resize", update);
+      window.removeEventListener("scroll", fixUnsetHeader);
+    };
   });
+
   return (
-    <div className={container}>
+    <div
+      className={`${container} ${!scrollingUp && !isTop && down} ${
+        scrollingUp && up
+      } ${menuOpen && extraPadding}`}
+    >
       <Logo />
       {isMobile && <MenuIcon />}
       {!isMobile && <Nav />}
