793c9edada12c0f9407f6a6a42f66acc4fd6f4be
Lakhte Ali Agha <lagha@ltvco.com>
2022-07-31 23:58:47 -0400

    header fix...again...

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -19,7 +19,7 @@ const Header = ({ isActuallyMobile }) => {
   };
 
   const fixUnsetHeader = () => {
-    if (window.scrollY > 80) {
+    if (window.scrollY > 150) {
       setIsTop(false);
     }
 
