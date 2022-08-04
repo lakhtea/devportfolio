5b523d4bf482b55b27aa17af091a4475b5d35ad7
Lakhte Ali Agha <lagha@ltvco.com>
2022-08-03 21:00:21 -0400

    finished hero page

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,11 +1,39 @@
 import styles from "./Hero.module.css";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import EmailIcon from "@mui/icons-material/Email";
 
 const Hero = () => {
-  const { container, message } = styles;
+  const { container, name, sub, blurb, link, icons, icon } = styles;
 
   return (
     <div id="Hero" className={`${container} hero`}>
-      <div className={message}>Hero</div>
+      <div>
+        <p className={name}>Lakhte Agha</p>
+        <p className={sub}>Software Engineer</p>
+      </div>
+
+      <p className={blurb}>
+        I am a software engineer specializing in creating seamless, efficient,
+        and accessible applications. Currently a full stack engineer, building a
+        variety of new features across multiple platforms at{" "}
+        <a
+          href="https://www.ltvco.com/"
+          target="_blank"
+          rel="noreferrer"
+          className={link}
+        >
+          The Lifetime Value Co.
+        </a>
+      </p>
+      <div className={icons}>
+        <a href="https://www.ltvco.com/" target="_blank" rel="noreferrer">
+          <GitHubIcon className={icon} />
+        </a>
+
+        <LinkedInIcon className={icon} />
+        <EmailIcon className={icon} />
+      </div>
     </div>
   );
 };
