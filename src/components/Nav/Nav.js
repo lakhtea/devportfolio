1ec5576d0e426e2114a6c82ab8e23844060ff094
Lakhte Ali Agha <lagha@ltvco.com>
2022-08-07 20:12:13 -0400

    header desktop

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,23 +1,52 @@
 import styles from "./Nav.module.css";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import EmailIcon from "@mui/icons-material/Email";
 
 const Nav = () => {
-  const { container, list, button } = styles;
+  const { container, list, icon, contact, link, listItem, icons } = styles;
 
-  const navLinks = [
-    { name: "About" },
-    { name: "Projects" },
-    { name: "Skills" },
-    { name: "Contact" },
-  ];
+  const navLinks = [{ name: "About" }, { name: "Projects" }];
 
   return (
     <div className={container}>
       <ul className={list}>
         {navLinks.map(({ name }) => (
-          <li key={name}>
-            <button className={button}>{name}</button>
+          <li className={listItem} key={name}>
+            <a href={`#${name}`}>{name}</a>
           </li>
         ))}
+        <li className={`${listItem} ${contact} menu-el`}>
+          <div className={icons}>
+            <a
+              className={link}
+              href="https://github.com/lakhtea"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <GitHubIcon className={icon} />
+              <span>GitHub</span>
+            </a>
+            <a
+              className={link}
+              href="https://www.linkedin.com/in/lakhte-agha-1909b11b2/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <LinkedInIcon className={icon} />
+              <span>LinkedIn</span>
+            </a>
+            <a
+              className={link}
+              href="mailto: lakhteagha@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <EmailIcon className={icon} />
+              <span>Email</span>
+            </a>
+          </div>
+        </li>
       </ul>
     </div>
   );
